d7d961b0563596d22bf1aa944faf511a9ce8b1eb
denbonte <dennis_bontempi@dfci.harvard.edu>
2023-02-17 11:43:16 +0100

    Add GitHub automation script

--- src/run_automation.py
+++ src/run_automation.py
@@ -1,6 +1,6 @@
 """
 -------------------------------------------------
-MHub - local automation for docker builds
+MHub - GitHub automation for docker builds
 -------------------------------------------------
 -------------------------------------------------
 Author: Dennis Bontempi
@@ -75,82 +75,13 @@ def push_docker_image(image_tag, verbose):
                             stderr=None if verbose else subprocess.DEVNULL)
 
 
-def get_dotgit_folder(path_to_folder):
-
-    # check the folder exists
-    if not os.path.exists(path_to_folder):
-        raise FileNotFoundError("Folder %s not found"%path_to_folder)
-    
-    # check if the specified folder is already a .git repository
-    # if not, check the .git repository exists and is a folder, otherwise raise an error
-    if ".git" in path_to_folder:
-        path_to_dotgit_folder = path_to_folder
-    else:
-        if not os.path.exists(os.path.join(path_to_folder, ".git")):
-            raise FileNotFoundError("Folder %s not found"%os.path.join(path_to_folder, ".git"))
-
-        path_to_dotgit_folder = os.path.join(path_to_folder, ".git")
-
-    return path_to_dotgit_folder
-
-
-## --------------------------------
-
-def get_git_hash(path_to_folder, source):
-
-    # check source is valid
-    # TO-DO: add checks on other branches? Does it even make sense?
-    if source not in ["HEAD", "origin/main"]:
-        raise ValueError("Source %s is not valid"%source)
-
-    path_to_dotgit_folder = get_dotgit_folder(path_to_folder)
-
-    # bash command to get the current commit hash
-    bash_command =  ["git",
-                     "--git-dir", "%s"%path_to_dotgit_folder,
-                     "rev-parse", "--verify", "HEAD"]
-
-    # run git in subprocess
-    process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
-    # get the output of the git command
-    output, error = process.communicate()
-    # decode the output
-    commit_hash = output.decode('utf-8').strip()
-
-    return commit_hash
-
-
-## --------------------------------
-
-def git_pull(path_to_folder):
-
-    path_to_dotgit_folder = get_dotgit_folder(path_to_folder)
-
-    # bash command to get the current commit hash
-    bash_command =  ["git",
-                     "--git-dir", "%s"%path_to_dotgit_folder,
-                     "pull"]
-
-    # run git in subprocess
-    process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
-    # get the output of the git command
-    output, error = process.communicate()
-
-    return output
-
-
 ## --------------------------------
 ## --------------------------------
 
 def main():
 
-    # TO-DO: implement ands set up logging
-    # https://stackoverflow.com/questions/7507825/where-is-a-complete-example-of-logging-config-dictconfig
-    #logging.config.dictConfig(config['logging'])
-    #logger = logging.getLogger(__name__)
-
     # parse command line arguments
-    parser = argparse.ArgumentParser(description='MHub - local automation for docker builds')
+    parser = argparse.ArgumentParser(description='MHub - GitHub automation for docker builds')
     #parser.add_argument('-l', '--logging', action='store_true', help='enable logging')
     parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose mode')
     parser.add_argument('--dryrun', action='store_true', help='execute in dry run mode')
@@ -162,24 +93,6 @@ def main():
     with open(args.config, 'r') as f:
         config_dict = yaml.safe_load(f)
     
-    # get hash for the current commit using git
-    # - using python:
-    #   https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
-    # - using subprocess:
-    #   https://stackoverflow.com/questions/949314/how-do-i-get-the-hash-for-the-current-commit-in-git
-    commit_hash = get_git_hash(path_to_folder = config_dict["github"]["repository_folder"],
-                               source = "HEAD")
-    remote_hash = get_git_hash(path_to_folder = config_dict["github"]["repository_folder"],
-                               source = "origin/main")
-
-    # if the two hashes are different, pull the latest changes
-    if commit_hash != remote_hash:
-        if args.dryrun:
-            print("git pull %s"%config_dict["github"]["repository_folder"])
-        else:
-            print(git_pull(path_to_folder = config_dict["github"]["repository_folder"]))
-
-
     # for every image in the config file, build the docker image and push it to the registry
     for image in config_dict["images"]:
         
@@ -198,8 +111,6 @@ def main():
         else:
             image_tag = build_docker_image(image_dict, verbose=args.verbose)
             push_docker_image(image_tag, verbose=args.verbose)
-
-        # TO-DO: add option to send an email with the output of the build and the logs
 
 
 if __name__ == '__main__':
