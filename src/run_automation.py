6e988c432a172695165dc56e55c602262365bcde
denbonte <dennis_bontempi@dfci.harvard.edu>
2023-02-17 12:00:53 +0100

    Add job to clone the models repo

--- src/run_automation.py
+++ src/run_automation.py
@@ -98,7 +98,7 @@ def main():
 
         # quick workaround to avoid passing the config dictionary to the `build_docker_image` function
         image_dict["repository_folder"] = config_dict["github"]["repository_folder"]
-        image_dict["dockerhub_username"] = config_dict["dockerhub"]["username"]
+        #image_dict["dockerhub_username"] = config_dict["dockerhub"]["username"]
 
         # TO-DO: add option to build only the images that have changed
         if args.dryrun:
@@ -107,7 +107,7 @@ def main():
             print("")
         else:
             image_tag = build_docker_image(image_dict, verbose=args.verbose)
-            push_docker_image(image_tag, verbose=args.verbose)
+            #push_docker_image(image_tag, verbose=args.verbose)
 
 
 if __name__ == '__main__':
